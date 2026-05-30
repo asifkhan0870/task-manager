c674ce4fcf3225df12cf8145b5ed0921c39e07c8
Asif khan <khanasif0870@gmail.com>
2026-05-30 16:30:59 +0530

    frontend production api

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://task-manager-hu0m.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
