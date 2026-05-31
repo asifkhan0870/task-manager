2e79ebe10a12e24e6b49cb2975b4aeb4fbbb2d67
Asif khan <khanasif0870@gmail.com>
2026-05-31 10:34:12 +0530

    adding in frontend backend url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://task-manager-nine-silk-78.vercel.app/",
+    baseURL: "https://task-manager-backend-4i6k.onrender.com/",
 });
 
 api.interceptors.request.use((config) => {
