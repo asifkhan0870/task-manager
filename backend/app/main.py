d49959b6223c84304333eab34a53bfbfb90bf834
Asif khan <khanasif0870@gmail.com>
2026-05-31 10:58:06 +0530

    fixing cors issue

--- backend/app/main.py
+++ backend/app/main.py
@@ -29,7 +29,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://task-manager-beta-three-71.vercel.app/",
+        "https://task-manager-beta-three-71.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
