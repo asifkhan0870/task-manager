e631fc42775f614ad74d8cca0d0029204c523426
Asif khan <khanasif0870@gmail.com>
2026-06-02 11:27:13 +0530

    seting url with backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    // baseURL: "https://task-manager-backend-4i6k.onrender.com/",
-    baseURL:"http://127.0.0.1:8000",
+    baseURL: "https://task-manager-backend-4i6k.onrender.com/",
+    // baseURL:"http://127.0.0.1:8000",
 });
 
 api.interceptors.request.use((config) => {
